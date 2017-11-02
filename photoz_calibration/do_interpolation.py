867ea633d52fc56e7daf9f74aa980cfe9e1e0b36
Tom McClintock <tmcclintock89@gmail.com>
2017-11-02 08:44:25 -0700

    The photoz code worked fine but I put in a bunch of print statements.

--- photoz_calibration/do_interpolation.py
+++ photoz_calibration/do_interpolation.py
@@ -2,31 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as interp
 plt.rc("text", usetex=True)
+plt.rc("font", size=18, family="serif")
 
 datapath = "sci_correction.dat"
-z, r, re = np.genfromtxt(datapath, unpack=True)
+z, Ratio, Ratioe = np.genfromtxt(datapath, unpack=True)
 
-rspl = interp.interp1d(z,r)# , kind="cubic")
-respl = interp.interp1d(z,re)#, kind="cubic")
+Ratiospl = interp.interp1d(z,Ratio)# , kind="cubic")
+Ratioespl = interp.interp1d(z,Ratioe)#, kind="cubic")
 
 
-zy1 = np.loadtxt("/home/tmcclintock/Desktop/des_wl_work/DATA_FILES/y1_data_files/Y1_meanz.txt")
+zy1 = np.loadtxt("Y1_meanz.txt")
 
 x = np.linspace(min(z), max(z), num=100)
-rf = rspl(x)
-ref = respl(x)
+Ratiof = Ratiospl(x)
+Ratioef = Ratioespl(x)
 
-ry1 = rspl(zy1)
-rey1 = respl(zy1)
-dp1 = 1./ry1
-dp1_err = rey1/ry1**2
+Ratioy1 = Ratiospl(zy1)
+Ratioey1 = Ratioespl(zy1)
+
+dp1 = 1./Ratioy1
+dp1_err = Ratioey1/Ratioy1**2
+print dp1_err[2,6]
+dp1_err = Ratioey1 * dp1**2
+print "err = ",dp1_err[2,6]
+print "var = ",dp1_err[2,6]**2
+
+print zy1[2, 6]
+print dp1[2, 6], 1./dp1[2,6]
+print Ratioey1[2, 6]
+
 np.savetxt("Y1_deltap1.txt", dp1)
 np.savetxt("Y1_deltap1_var.txt", dp1_err**2)
 
-plt.errorbar(z, r, re)
-plt.plot(x, rf, c='k')
-plt.fill_between(x, rf-ref, rf+ref, color='k', alpha=0.3)
-plt.errorbar(zy1.flatten(), ry1.flatten(), rey1.flatten(), ls='', marker='.', c='r')
+plt.errorbar(z, Ratio, Ratioe)
+plt.plot(x, Ratiof, c='k')
+plt.fill_between(x, Ratiof-Ratioef, Ratiof+Ratioef, color='k', alpha=0.3)
+plt.errorbar(zy1.flatten(), Ratioy1.flatten(), Ratioey1.flatten(), ls='', marker='.', c='r')
 plt.ylabel(r"$(1+\delta)^{-1} = \Sigma_{crit,true}^{-1}/\Sigma_{crit,mof}^{-1}$")
 plt.xlabel("redshift")
-plt.show()
+plt.subplots_adjust(bottom=0.15, left=0.15)
+#plt.show()
