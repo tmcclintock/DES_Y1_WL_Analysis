Update with correct units all around.

lensing_analysis/analysis_script.py
@@ -5,6 +5,7 @@ import os, sys
 
 def test_call(args, bfpath=None, testbf=False):
     guess = get_model_start(model_name, args['lam'], args['h'])
+    guess[0] = np.log10(4.592486e+14)
     if testbf:
         print "Testing bestfit"
         guess = np.loadtxt(bfpath) #Has everything
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     useJK = False
 
     #Loop over bins
-    zhi, zlo = 1, -1
+    zhi, zlo = 2, -1
     lhi, llo = 6, 2
     for i in xrange(zhi, zlo, -1):#z bins #only 2,1,0 for y1 and sv but 3,2,1,0 for cal
         for j in xrange(lhi, llo, -1): #lambda bins

lensing_analysis/helper_functions.py
@@ -180,9 +180,9 @@ def get_args(model_name, zi, lj, name="Y1", covkind="SAC", blinded=True, cuts=[0
     #NEW - read in the orientation data, index it appropriately, and save
     #Update - now, we need to create a spline for X(R) and evaluate it at Rp
     Rp = np.logspace(-2, 2.4, 1000, base=10) #Mpc/h comoving projected
-    Rx, X, Xerr = np.loadtxt(X_ratio_path%(zi, lj), unpack=True)
-    Xspl = IUS(np.log(Rx), X, ext=3) #ext=3 means extrapolation returns the boundary value
-    args["X_Sigma_ratio_all_Rp"] = Xspl(np.log(Rp))
+    #Rx, X, Xerr = np.loadtxt(X_ratio_path%(zi, lj), unpack=True)
+    #Xspl = IUS(np.log(Rx), X, ext=3) #ext=3 means extrapolation returns the boundary value
+    #args["X_Sigma_ratio_all_Rp"] = Xspl(np.log(Rp))
     #inds = args["lensing_kept_indices"]
     #X = X[inds]
     #Xerr = Xerr[inds]